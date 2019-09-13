50a0c26498a959e3f8729460085a1750d8b054f0
T0588 <kiran.bal@triassicsolutions.com>
2019-09-13 17:36:32 +0530

    new changes

---
+++ shopping_database/queries_create/create_table1.sql
@@ -0,0 +1,6 @@
+CREATE TABLE products
+(product_id VARCHAR(10) PRIMARY KEY,
+product_name VARCHAR(30),
+product_category VARCHAR(20),
+product_price FLOAT,
+product_description TINYTEXT);
--- tryncatch.sql
+++
@@ -1,6 +0,0 @@
-BEGIN TRY
-	SELECT *  FROM mytable;
-END TRY
-BEGIN CATCH
-	SELECT *  FROM mytable;
-END CATCH;
