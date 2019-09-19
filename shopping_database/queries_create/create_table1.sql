aa8f60ae182a00d9b45133fdadaeaeeeb02cb833
k4kiran <kforkiran005@gmail.com>
2019-09-19 08:12:45 +0530

    new

--- shopping_database/queries_create/create_table1.sql
+++ shopping_database/queries_create/create_table1.sql
@@ -1,6 +1,6 @@
-CREATE TABLE products
-(product_id VARCHAR(10) PRIMARY KEY,
-product_name VARCHAR(30),
-product_category VARCHAR(20),
-product_price FLOAT,
-product_description TINYTEXT);
+CREATE TABLE customer
+(customer_id INT AUTO_INCREMENT PRIMARY KEY,
+first_name VARCHAR(50),
+last_name VARCHAR(50),
+address_id INT,
+email VARCHAR(20));
